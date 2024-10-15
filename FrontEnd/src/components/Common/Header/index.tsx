bc4ce11902c79902c2dfc98100762187585f8969
Jopereira2005 <jopereiravieira98@gmail.com>
2024-10-15 14:56:03 -0300

    add scss features and icons

--- FrontEnd/src/components/Common/Header/index.tsx
+++ FrontEnd/src/components/Common/Header/index.tsx
@@ -1,21 +1,17 @@
-import style from './style.module.scss'
-import logo from '../../../assets/Home/logo.svg'
-import profile from '../../../assets/Home/profile.svg'
+import styled from './style.module.scss'
+import logo from '../../../assets/Header_Sidebar/logo.svg'
+import { ProfileIcon } from '../../../assets/Header_Sidebar/Profile'
 
 const Header = () => {
   return (
-    <>
-      <header className={ style.header }>
-        <div className={ style.header__logo }>
-          <img src={ logo } className={ style.header__logo__img } alt="logo" />
-          <h1 className={ style.header__logo__text }>Own<span>Light</span></h1>
-        </div>
-
-        <div className={ style.header__profile }>
-          <img src={ profile } className={ style.header__logo__img } alt="profile" />
-        </div>
-      </header>
-    </>
+    <header className={ styled.header }>
+      <div className={ styled.header__logo }>
+        <img src={ logo } className={ styled.header__logo__img } alt="logo" />
+        <h1 className={ styled.header__logo__text }>Own<span>Light</span></h1>
+      </div>
+      
+      <ProfileIcon className={ styled.header__profile }/>
+    </header>
   )
 }
 
