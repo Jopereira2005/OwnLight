41b55d5e32d087df075e3eb7cedbd4bf4a9affa7
Jopereira2005 <jopereiravieira98@gmail.com>
2024-10-18 07:36:05 -0300

    Sidebar feature

--- FrontEnd/src/components/Common/Header/index.tsx
+++ FrontEnd/src/components/Common/Header/index.tsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
+
 import styled from './style.module.scss'
+
 import logo from '../../../assets/Header_Sidebar/logo.svg'
-import { ProfileIcon } from '../../../assets/Header_Sidebar/Profile'
+import ProfileIcon from '../../../assets/Header_Sidebar/Profile.svg'
+
+import Sidebar from '../SideBar';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false); // Estado para controlar abertura
+
+  // Função para alternar entre aberto e fechado
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className={ styled.header }>
-      <div className={ styled.header__logo }>
-        <img src={ logo } className={ styled.header__logo__img } alt="logo" />
-        <h1 className={ styled.header__logo__text }>Own<span>Light</span></h1>
-      </div>
-      
-      <ProfileIcon className={ styled.header__profile }/>
-    </header>
+    <>
+      <header className={ styled.header }>
+        <div className={ styled.header__logo }>
+          <img src={ logo } className={ styled.header__logo__img } alt="logo" />
+          <h1 className={ styled.header__logo__text }>Own<span>Light</span></h1>
+        </div>
+
+        <div className={ styled.header__profile }>
+          <img onClick={ toggleSidebar }src={ ProfileIcon } className={ styled.header__logo__img } alt="profile" />
+        </div>
+      </header>
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+    </>
   )
 }
 
