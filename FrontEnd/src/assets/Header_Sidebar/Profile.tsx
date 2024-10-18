041b990735bd07bd2ffdde506e93401cf7b3ce4c
Jopereira2005 <jopereiravieira98@gmail.com>
2024-10-18 07:34:21 -0300

    fix icons

--- FrontEnd/src/assets/Header_Sidebar/Profile.tsx
+++
@@ -1,5 +0,0 @@
-import type { SVGProps } from 'react';
-
-export function ProfileIcon(props: SVGProps<SVGSVGElement>) {
-	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5}><path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10S17.523 2 12 2"></path><path d="M4.271 18.346S6.5 15.5 12 15.5s7.73 2.846 7.73 2.846M12 12a3 3 0 1 0 0-6a3 3 0 0 0 0 6"></path></g></svg>);
-}
