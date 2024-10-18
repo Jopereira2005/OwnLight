ceb35fb837286ad412f430f1aec0d6e85953d3df
Jopereira2005 <jopereiravieira98@gmail.com>
2024-10-18 07:37:06 -0300

    small changes

--- FrontEnd/src/pages/Login/index.tsx
+++ FrontEnd/src/pages/Login/index.tsx
@@ -1,27 +1,26 @@
-import style from'./style.module.scss';
+import styled from'./style.module.scss';
 import Input from '../../components/Login_Register/Input'
 import logo from '../../assets/Login_Register/logo.svg'
 import facebook_icon from '../../assets/Login_Register/facebook.svg'
 import google_icon from '../../assets/Login_Register/google.svg'
 import x_icon from '../../assets/Login_Register/x.svg'
 
-
 function Login() {
   return (
-    <div className={ style.login }>
-      <div className={ style.background }>
-        <main className={ style.main }>
-          <div className={ style.main__welcome }>
-            <img src={ logo } alt="logo" className={ style.main__welcome__logo } />
-            <h1 className={ style.main__welcome__text }>Bem-vindo de volta ao <span>OwnLight!</span></h1>
+    <div className={ styled.login }>
+      <div className={ styled.background }>
+        <main className={ styled.main }>
+          <div className={ styled.main__welcome }>
+            <img src={ logo } alt="logo" className={ styled.main__welcome__logo } />
+            <h1 className={ styled.main__welcome__text }>Bem-vindo de volta ao <span>OwnLight!</span></h1>
           </div>
 
-          <div className={ style.main__div }></div>
+          <div className={ styled.main__div }></div>
 
-          <div className={ style.main__login }>
-            <h1 className={ style.main__login__title }>Login</h1>
-            <div className={ style.main__login__form }>
-              <div className={ style.main__login__form__inputs }>
+          <div className={ styled.main__login }>
+            <h1 className={ styled.main__login__title }>Login</h1>
+            <div className={ styled.main__login__form }>
+              <div className={ styled.main__login__form__inputs }>
                 <Input
                   type="text"
                   name="Nome"
@@ -32,19 +31,19 @@ function Login() {
                   name="Senha"
                 />
               </div>
-              <a href="#" className={ style.main__login__link }>Esqueceu a senha?</a>
+              <a href="#" className={ styled.main__login__link }>Esqueceu a senha?</a>
             </div>
             
-            <button className={ style.main__login__button }>Entrar</button>
-            <a href="#" className={ style.main__login__link }>Criar conta</a>
+            <button className={ styled.main__login__button }>Entrar</button>
+            <a href="#" className={ styled.main__login__link }>Criar conta</a>
           </div>
 
-          <div className={ style.main__social_medias }>
-            <p className={ style.main__social_medias__text }>Entrar com</p>
-            <div className={ style.main__social_medias__imgs }>
-              <img src={ facebook_icon } alt="facebook" className={ style.main__social_medias__imgs } />
-              <img src={ google_icon } alt="google" className={ style.main__social_medias__imgs } />
-              <img src={ x_icon } alt="x" className={ style.main__social_medias__imgs } />
+          <div className={ styled.main__social_medias }>
+            <p className={ styled.main__social_medias__text }>Entrar com</p>
+            <div className={ styled.main__social_medias__imgs }>
+              <img src={ facebook_icon } alt="facebook" className={ styled.main__social_medias__imgs } />
+              <img src={ google_icon } alt="google" className={ styled.main__social_medias__imgs } />
+              <img src={ x_icon } alt="x" className={ styled.main__social_medias__imgs } />
             </div>
           </div>  
         </main>
--- FrontEnd/src/pages/Register/index.tsx
+++ FrontEnd/src/pages/Register/index.tsx
@@ -1,27 +1,26 @@
-import style from'./style.module.scss';
+import styled from'./style.module.scss';
 import Input from '../../components/Login_Register/Input'
 import logo from '../../assets/Login_Register/logo.svg'
 import facebook_icon from '../../assets/Login_Register/facebook.svg'
 import google_icon from '../../assets/Login_Register/google.svg'
 import x_icon from '../../assets/Login_Register/x.svg'
 
-
 function Register() {
   return (
-    <div className={ style.login }>
-      <div className={ style.background }>
-        <main className={ style.main }>
-          <div className={ style.main__welcome }>
-            <img src={ logo } alt="logo" className={ style.main__welcome__logo } />
-            <h1 className={ style.main__welcome__text }>Bem-vindo ao <span>OwnLight!</span></h1>
+    <div className={ styled.login }>
+      <div className={ styled.background }>
+        <main className={ styled.main }>
+          <div className={ styled.main__welcome }>
+            <img src={ logo } alt="logo" className={ styled.main__welcome__logo } />
+            <h1 className={ styled.main__welcome__text }>Bem-vindo ao <span>OwnLight!</span></h1>
           </div>
 
-          <div className={ style.main__div }></div>
+          <div className={ styled.main__div }></div>
 
-          <div className={ style.main__register }>
-            <h1 className={ style.main__register__title }>Cadastro</h1>
-            <div className={ style.main__register__form }>
-              <div className={ style.main__register__form__inputs }>
+          <div className={ styled.main__register }>
+            <h1 className={ styled.main__register__title }>Cadastro</h1>
+            <div className={ styled.main__register__form }>
+              <div className={ styled.main__register__form__inputs }>
                 <Input
                   type="text"
                   name="Nome"
@@ -49,15 +48,15 @@ function Register() {
               </div>
             </div>
             
-            <button className={ style.main__register__button }>Entrar</button>
+            <button className={ styled.main__register__button }>Entrar</button>
           </div>
 
-          <div className={ style.main__social_medias }>
-            <p className={ style.main__social_medias__text }>Entrar com</p>
-            <div className={ style.main__social_medias__imgs }>
-              <img src={ facebook_icon } alt="facebook" className={ style.main__social_medias__imgs } />
-              <img src={ google_icon } alt="google" className={ style.main__social_medias__imgs } />
-              <img src={ x_icon } alt="x" className={ style.main__social_medias__imgs } />
+          <div className={ styled.main__social_medias }>
+            <p className={ styled.main__social_medias__text }>Entrar com</p>
+            <div className={ styled.main__social_medias__imgs }>
+              <img src={ facebook_icon } alt="facebook" className={ styled.main__social_medias__imgs } />
+              <img src={ google_icon } alt="google" className={ styled.main__social_medias__imgs } />
+              <img src={ x_icon } alt="x" className={ styled.main__social_medias__imgs } />
             </div>
           </div>  
         </main>
