4a9b7e8948669d3750c55a3f7c7a7c682b99d1f9
Jopereira2005 <jopereiravieira98@gmail.com>
2024-10-28 17:23:42 -0300

    create input toggle

---
+++ FrontEnd/src/components/Common/Switch/index.tsx
@@ -0,0 +1,21 @@
+import styled from './style.module.scss'
+
+import { OnIcon } from "../../../assets/Cards/On"
+
+interface SwichProp {
+  state: boolean,
+  toggleSwitch: () => void
+}
+
+const Switch = ({state, toggleSwitch}: SwichProp) => {
+  return (
+    <div className={`${styled.switch} ${state ? styled.switch__active : '' }`} onClick={ toggleSwitch }>
+      <span className={ styled.switch__text } >{state ? "ON" : "OFF"}</span>
+      <div className={`${ styled.switch__icon } ${state ? styled.switch__icon__on : ""}`}>
+        <OnIcon className={ styled.switch__icon__svg }/>
+      </div>
+    </div>
+  )
+}
+
+export default Switch
