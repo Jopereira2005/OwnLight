99295a1cbbf9d1996d5e33c6b043b447b2cd4385
Jopereira2005 <jopereiravieira98@gmail.com>
2024-11-05 11:11:13 -0300

    add icons

---
+++ FrontEnd/src/assets/Cards/More.tsx
@@ -0,0 +1,5 @@
+import type { SVGProps } from 'react';
+
+export function MoreIcon(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}><g fill="none"><path stroke="currentColor" strokeLinejoin="round" strokeWidth={4} d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"></path><circle cx={14} cy={24} r={3} fill="currentColor"></circle><circle cx={24} cy={24} r={3} fill="currentColor"></circle><circle cx={34} cy={24} r={3} fill="currentColor"></circle></g></svg>);
+}
---
+++ FrontEnd/src/assets/Home/Add.tsx
@@ -0,0 +1,5 @@
+import type { SVGProps } from 'react';
+
+export function AddIcon(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="currentColor" fillRule="evenodd" clipRule="evenodd"><path d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12m10-8a8 8 0 1 0 0 16a8 8 0 0 0 0-16"></path><path d="M13 7a1 1 0 1 0-2 0v4H7a1 1 0 1 0 0 2h4v4a1 1 0 1 0 2 0v-4h4a1 1 0 1 0 0-2h-4z"></path></g></svg>);
+}
