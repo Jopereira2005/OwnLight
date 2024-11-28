a3a160764c90eb8e773ff8b4490fa6bf31e47228
Jopereira2005 <jopereiravieira98@gmail.com>
2024-11-27 22:34:05 -0300

    routine route

--- FrontEnd/src/routes.tsx
+++ FrontEnd/src/routes.tsx
@@ -1,16 +1,18 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Routine from "./pages/Routine";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+
 
 
 function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> }/>
+      <Route path="/rotina" element={ <Routine /> }/>
       <Route path="/cadastro" element={ <Register /> }/>
       <Route path="/login" element={ <Login /> }/>
-
     </Routes>
   );
 }
