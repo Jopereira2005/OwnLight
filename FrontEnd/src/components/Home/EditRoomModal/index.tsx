1036bf5441581670de997cf6850ba1d4bb9139c7
Jopereira2005 <jopereiravieira98@gmail.com>
2024-12-04 22:15:15 -0300

    edit room modal

---
+++ FrontEnd/src/components/Home/EditRoomModal/index.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+
+import styled from './style.module.scss'
+
+import { Room } from '../../../interfaces/Room'
+
+import { TrashIcon } from '../../../assets/Home/Trash'
+
+interface EditRoomModalProps {
+  room: Room,
+  isOpen: boolean,
+  toggleEditRoomModal: () => void,
+  onSubmit: (dados: FormData | number) => void,
+}
+
+const EditRoomModal = ({ room, isOpen, toggleEditRoomModal, onSubmit }: EditRoomModalProps) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("no_scroll");
+    } else {
+      document.body.classList.remove("no_scroll");
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      setDefaultData();
+    }
+  }, [isOpen == true]);
+
+  const [name, setName] = useState('');
+
+  const setDefaultData = () => {
+    setName(room.name ?? '');
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    onSubmit(formData);
+    toggleEditRoomModal();
+  }
+  
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    onSubmit(room.id_room ?? 0);
+    toggleEditRoomModal();
+  }
+
+  return (
+    <>
+      <div className={ `${styled.modal} ${isOpen ? styled.modal__open : styled.modal__closed}` }>
+        <h1 onClick={ setDefaultData } className={ styled.modal__title }>Editar Ambiente</h1>
+        <form onSubmit={ handleSubmit } className={ styled.modal__form }>
+          <div className={ styled.modal__form__container_input}>
+            <div className={ styled.modal__form__container_input__input}>
+              <label htmlFor="nome">Nome: </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={ name }
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Digite o nome do dispositivo..."
+                required
+                autoComplete="off"
+              /> 
+            </div>
+          </div>
+
+          <div className={ styled.modal__form__button_group }>
+            <button className={ styled.modal__form__button_group__button_trash } onClick={ handleDelete }><TrashIcon className={styled.modal__form__button_group__button_trash__icon }/></button>
+            <button type="submit" className={ styled.modal__form__button_group__button }>Editar</button>
+          </div>
+        </form>
+      </div>
+      { isOpen && <div className={ styled.backdrop } onClick={ toggleEditRoomModal }></div> }
+    </> 
+  )
+}
+
+export default EditRoomModal
