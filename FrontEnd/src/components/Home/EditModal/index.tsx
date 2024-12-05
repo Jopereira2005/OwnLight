b7f131cfb05b556f8ff551a790ff67f9135ef563
Jopereira2005 <jopereiravieira98@gmail.com>
2024-12-04 22:14:46 -0300

    edit device modal

--- FrontEnd/src/components/Home/EditModal/index.tsx
+++ FrontEnd/src/components/Home/EditModal/index.tsx
@@ -7,16 +7,17 @@ import InputSelect from '../../Common/InputSelect';
 import { Room } from '../../../interfaces/Room'
 import { Device } from '../../../interfaces/Device'
 
+import { TrashIcon } from '../../../assets/Home/Trash'
 
-interface CreateModalProps {
+interface EditModalProps {
   device: Device,
   rooms: Room[],
   isOpen: boolean,
   toggleEditModal: () => void,
-  onSubmit: (dados: FormData) => void,
+  onSubmit: (dados: FormData | number) => void,
 }
 
-const EditModal = ({ device, rooms, isOpen, toggleEditModal, onSubmit }: CreateModalProps) => {
+const EditModal = ({ device, rooms, isOpen, toggleEditModal, onSubmit }: EditModalProps) => {
   useEffect(() => {
     if (isOpen) {
       document.body.classList.add("no_scroll");
@@ -34,26 +35,30 @@ const EditModal = ({ device, rooms, isOpen, toggleEditModal, onSubmit }: CreateM
   const [name, setName] = useState('');
   const [isDimmable, setIsDimmable] = useState(false);
   const [roomId, setRoomId] = useState<string | number | null>(null);
+  const [brightness, setBrightness] = useState(100);
 
   const options = rooms.map((room) => ({
     value: room.id_room!,
     label: room.name,
   }));
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    onSubmit(formData);
-    handleClose();
-  }
-
   const setDefaultData = () => {
     setName(device.name ?? '');
     setIsDimmable(device.is_dimmable ?? false);
     setRoomId(device.id_room ?? '');
+    setBrightness(device.brightness ?? 100);
   }
 
-  const handleClose = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    onSubmit(formData);
+    toggleEditModal();
+  }
+  
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    onSubmit(device.id_device ?? 0);
     toggleEditModal();
   }
 
@@ -77,7 +82,7 @@ const EditModal = ({ device, rooms, isOpen, toggleEditModal, onSubmit }: CreateM
               /> 
             </div>
 
-            <div className={ styled.modal__form__container_input__switch}>
+            <div className={ styled.modal__form__container_input__switch }>
               <h2 className={ styled.modal__form__container_input__switch__text }>Iluminação Ajustável</h2>
               <label className={ styled.modal__form__container_input__switch__input }>
                 <input 
@@ -89,6 +94,20 @@ const EditModal = ({ device, rooms, isOpen, toggleEditModal, onSubmit }: CreateM
                 />
                 <span className={ styled.modal__form__container_input__switch__slider }></span>
               </label>
+            </div>
+
+            <div className={ styled.modal__form__container_input__range }>
+              <label className={ !isDimmable ? styled.modal__form__container_input__range__disable : undefined } htmlFor="brightness" >Brilho: { brightness }</label>
+              <input
+                id="brightness"
+                name="brightness"
+                type="range" 
+                min="1" 
+                max="100" 
+                value={ brightness }
+                onChange={(e) => setBrightness(Number(e.target.value))}
+                disabled={ !isDimmable }  
+              />
             </div>
 
             <div className={ styled.modal__form__container_input__select }>
@@ -105,7 +124,10 @@ const EditModal = ({ device, rooms, isOpen, toggleEditModal, onSubmit }: CreateM
             </div>
           </div>
 
-          <button type="submit" className={ styled.modal__form__button }>Editar</button>
+          <div className={ styled.modal__form__button_group }>
+            <button className={ styled.modal__form__button_group__button_trash } onClick={ handleDelete }><TrashIcon className={styled.modal__form__button_group__button_trash__icon }/></button>
+            <button type="submit" className={ styled.modal__form__button_group__button }>Editar</button>
+          </div>
         </form>
       </div>
       { isOpen && <div className={ styled.backdrop } onClick={ toggleEditModal }></div> }
