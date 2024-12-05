56357c8253522929328ae187a466a5ee2e05774e
Jopereira2005 <jopereiravieira98@gmail.com>
2024-12-04 22:15:34 -0300

    routine interface

--- FrontEnd/src/interfaces/Routine.ts
+++ FrontEnd/src/interfaces/Routine.ts
@@ -1,9 +1,10 @@
-// import { User } from './User'
-// import { Room } from './Room'
-// import { Group } from './Group'
+import { User } from './User'
+import { Room } from './Room'
+import { Group } from './Group'
 
-// export interface Routine {
-//   id_routine?: number,
-//   name: string,
-//   action:
-// }
+export interface Routine {
+  id_routine?: number,
+  name: string,
+  id_target?: number,
+  target_type: string
+}
